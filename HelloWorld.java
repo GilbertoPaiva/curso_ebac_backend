d59684c3fddca91d738b5e0c07acdc2adbfe3872
Gilberto <gilberto.melo.paiva@gmail.com>
2024-04-28 20:21:49 -0300

    Primeiro commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String ambiente = args[0];
+
+        if (ambiente.equals("DEV")) {
+            System.out.print(ambiente);
+        }
+    }
+}
